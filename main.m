f1801739b7593ea0d7386e1ccf1da12c241d2328
tingleshao <tingleshao@gmail.com>
2014-08-14 22:21:38 -0400

    first blood

---
+++ main.m
@@ -0,0 +1,14 @@
+% Implementation of the flux tensor algorithm for motion detection in a
+% video.
+
+% paper: Bunyak, Palaniappan, Nath: "FLux Tensor Constrained Geodesic Active Contours with Sensor Fusion for Persistent Object Tracking"
+
+% steps: 
+% for imcoming input video v, take gradient on only & both x, y dirs 
+% and take gradient on t dir
+% and integrate 
+% finally compute the trace.. .
+
+
+
+
